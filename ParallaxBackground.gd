d671879ab2fbc5412a9fa047dfdc2cddb9fe8039
Tobi <t.walter@raumobil.com>
2023-02-05 13:23:11 +0100

    feat: add constant speed increase
    Increase the speed of the background every 10s by a coefficient of 0.5 to increase the challengeby time.

--- ParallaxBackground.gd
+++ ParallaxBackground.gd
@@ -1,4 +1,10 @@
 extends ParallaxBackground
 
+var speed_coefficient = 1.0
+const SPEED_INCRESE_COEFFICENT = 0.5
+
 func _process(delta):
-	scroll_offset.y -= 400 * delta
+	scroll_offset.y -= 400 * delta * speed_coefficient
+
+func _on_IncreaseSpeedTimer_timeout():
+	speed_coefficient+= SPEED_INCRESE_COEFFICENT
